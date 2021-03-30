834a015df630adf74ac96dabfbe7ae01c132701b
Josh Holmer <jholmer.in@gmail.com>
2021-03-30 09:32:08 -0400

    Bump for rav1e compatibility

--- rav1e-by-gop/src/encode/stats.rs
+++ rav1e-by-gop/src/encode/stats.rs
@@ -1180,9 +1180,9 @@ pub struct SerializableEncoderStats {
     /// Stores count of pixels belonging to each transform type in this frame
     pub tx_type_counts: [usize; TX_TYPES],
     /// Stores count of pixels belonging to each luma prediction mode in this frame
-    pub luma_pred_mode_counts: ArrayVec<[usize; PREDICTION_MODES]>,
+    pub luma_pred_mode_counts: ArrayVec<usize, PREDICTION_MODES>,
     /// Stores count of pixels belonging to each chroma prediction mode in this frame
-    pub chroma_pred_mode_counts: ArrayVec<[usize; PREDICTION_MODES]>,
+    pub chroma_pred_mode_counts: ArrayVec<usize, PREDICTION_MODES>,
 }
 
 impl From<&EncoderStats> for SerializableEncoderStats {
